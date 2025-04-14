fbed49598d3905e06493e173b43822334d7f5fc4
alkrivosheev <alkrivosheev@gmail.com>
2025-04-14 14:22:53 +0300

    init

---
+++ src/test/java/ru/job4j/sqch9ex1/SqCh9Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.sqch9ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh9Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
