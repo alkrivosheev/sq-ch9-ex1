9cadbbc9f45a98aa6dc7f52437a3f5e5e636be67
alkrivosheev <alkrivosheev@gmail.com>
2025-04-14 15:39:08 +0300

    Application vision Bean - add counter

--- src/main/java/ru/job4j/sqch9ex1/LoginProcessor.java
+++ src/main/java/ru/job4j/sqch9ex1/LoginProcessor.java
@@ -3,21 +3,29 @@ package ru.job4j.sqch9ex1;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 import ru.job4j.sqch9ex1.services.LoggedUserManagementService;
+import ru.job4j.sqch9ex1.services.LoginCountService;
 
 
 @Component
 @RequestScope
 public class LoginProcessor {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(
+            LoggedUserManagementService loggedUserManagementService,
+            LoginCountService loginCountService
+    ) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
+        loginCountService.increment();
+
         String username = this.username;
         String password = this.password;
 
--- src/main/java/ru/job4j/sqch9ex1/controller/MainController.java
+++ src/main/java/ru/job4j/sqch9ex1/controller/MainController.java
@@ -5,13 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.sqch9ex1.services.LoggedUserManagementService;
+import ru.job4j.sqch9ex1.services.LoginCountService;
 
 @Controller
 public class MainController {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -24,10 +27,15 @@ public class MainController {
         }
 
         String username = loggedUserManagementService.getUserName();
+        int count = loginCountService.getCount();
 
         if (username == null) {
             return "redirect:/";
         }
+
+        model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
+
         return "main.html";
     }
 }
